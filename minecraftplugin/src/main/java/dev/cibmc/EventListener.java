88add18d803fccbac85c6664dcdf89950e33038d
BuildTools <unconfigured@null.spigotmc.org>
2021-12-23 14:36:45 -0500

    stuff

--- minecraftplugin/src/main/java/dev/cibmc/EventListener.java
+++ minecraftplugin/src/main/java/dev/cibmc/EventListener.java
@@ -3,7 +3,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventListener implements Listener
