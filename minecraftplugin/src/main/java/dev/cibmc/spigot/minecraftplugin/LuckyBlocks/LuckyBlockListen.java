6cc84e72c0ef76cff4dc58c63a43546aef5ec84f
BuildTools <unconfigured@null.spigotmc.org>
2021-12-23 18:18:33 -0500

    fixed enchantment application

--- minecraftplugin/src/main/java/dev/cibmc/spigot/minecraftplugin/LuckyBlocks/LuckyBlockListen.java
+++ minecraftplugin/src/main/java/dev/cibmc/spigot/minecraftplugin/LuckyBlocks/LuckyBlockListen.java
@@ -1,13 +1,18 @@
 package dev.cibmc.spigot.minecraftplugin.LuckyBlocks;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Random;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.entity.Cow;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.Listener;
@@ -15,10 +20,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MainHand;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class LuckyBlockListen implements Listener
 {
-    String[] enchs = {"ENCHANTMENT.DAMAGE_ALL", "ENCHANTMENT.DURABILITY", "ENCHANTMENT.DAMAGE_ARTHOPODS", "ENCHANTMENT.DAMAGE_UNDEAD", "ENCHANTMENT.LOOT_BONUS_MOBS", "ENCHANTMENT.FIRE_ASPECT", "ENCHANTMENT.MENDING"};
+    String[] enchs = {"DAMAGE_ALL", "DURABILITY", "DAMAGE_ARTHOPODS", "DAMAGE_UNDEAD", "LOOT_BONUS_MOBS", "FIRE_ASPECT", "MENDING"};
+    //String[] enchs = {"SHARPNESS", "UNBREAKING", "BANE_OF_ARTHOPODS", "SMITE", "LOOTING", "FIRE_ASPECT", "MENDING"};
     
     @EventHandler
     public void onBreak(BlockBreakEvent event)
@@ -42,6 +50,7 @@ public class LuckyBlockListen implements Listener
                 cow.setPassenger(villager);
                 villager.setGravity(false);
                 cow.setGravity(false);
+                cow.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 40, 3));
                 villager.setFireTicks(Integer.MAX_VALUE);
                 break;
             case 1:
@@ -52,40 +61,38 @@ public class LuckyBlockListen implements Listener
                 addEnch(sword);
                 Zombie zombie = location.getWorld().spawn(location, Zombie.class);
                 zombie.getEquipment().setItemInMainHand(sword);
+                break;
             case 3:
-                location.getBlock().setType(Material.TNT);
-                Location fire = event.getBlock().getLocation().add(0, 1, 0);
-                fire.getBlock().setType(Material.FIRE);
+                for(int i = 0; i < 5; i++)
+                {
+                    location.getWorld().spawn(location, Creeper.class);
+                }
+                break;
                 
         }
     }
 
-    public void addEnch(ItemStack stack)
+    public void addEnch(ItemStack item) 
     {
-        ArrayList<Integer> addedEnch = new ArrayList<>();
-        for(int i = 0; i < 3; i++)
+        ArrayList<Enchantment> possible = new ArrayList<Enchantment>();
+        Random rand = new Random();
+     
+        for (Enchantment ench : Enchantment.values()) 
         {
-            Random rand = new Random();
-            int randEnch = rand.nextInt(6);
-            int level = rand.nextInt(5) + 1;
-            while(addedEnch.contains(randEnch))
+            if (ench.canEnchantItem(item)) 
             {
-                randEnch = rand.nextInt(6);
-            }
-            addedEnch.add(randEnch);
-            if(enchs[randEnch].equals("ENCHANTMENT.FIRE_ASPECT"))
-            {
-                level = rand.nextInt(2) + 1;
-                stack.addEnchantment(Enchantment.getByName(enchs[randEnch]), level);
-            }
-            else if(enchs[randEnch].equals("ENCHANTMENT.MENDING"))
-            {
-                stack.addEnchantment(Enchantment.getByName(enchs[randEnch]), 1);
-            }
-            else
-            {
-                stack.addEnchantment(Enchantment.getByName(enchs[randEnch]), level);
+                possible.add(ench);
             }
         }
+     
+        for(int i = 0; i < 3; i++)
+        {
+            if (possible.size() >= 1) 
+            {
+            Collections.shuffle(possible);
+            Enchantment chosen = possible.get(0);
+            item.addEnchantment(chosen, rand.nextInt(chosen.getMaxLevel()) + 1);
+        }
+    }
     }
 }
